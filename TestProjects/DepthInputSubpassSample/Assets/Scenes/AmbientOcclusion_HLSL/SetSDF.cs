c4b8ff3c56631850738663d1604d004a1c579557
Yang Qi <yangq@meta.com>
2024-11-18 11:41:08 -0800

    Add readme

--- TestProjects/DepthInputSubpassSample/Assets/Scenes/AmbientOcclusion_HLSL/SetSDF.cs
+++ TestProjects/DepthInputSubpassSample/Assets/Scenes/AmbientOcclusion_HLSL/SetSDF.cs
@@ -13,6 +13,10 @@ public class SetSDF : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update the position
+        transform.position = new Vector3(Mathf.Cos(Time.time) + 0.7f, transform.position.y, transform.position.z);
+
+        // Set the position and the radius to shader
         AO_Box.GetComponent<Renderer>().sharedMaterial.SetVector("_Position", transform.position);
         AO_Box.GetComponent<Renderer>().sharedMaterial.SetFloat("_Raduis", transform.localScale.x / 2);
     }
